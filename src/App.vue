60232dc689b6d9ddfc36a8faa35f19f99075ecf1
phonchick <mehmetcankz78@hotmail.com>
2024-03-06 10:53:59 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<script>
+  import ShoppingCartItem from '/src/components/ShoppingCartItem.vue'
+  export default {
+  name: "app",
+  components: {
+    ShoppingCartItem
+  },
+  data() {
+    return {
+      inShoppingCartItems: [
+        {name: 'Örnek ürün', price: 40, quantity: 1, discount: 0, isOpen: false}
+      ],
+      openTab: 1
+    }
+  },
+  methods: {
+    toggleTabs: function(tabNumber){
+      this.openTab = tabNumber
+    }
+  }
+}
+</script>
+<template>
+  <!--no print area-->
+  <div class="hide-print flex flex-row h-screen antialiased text-blue-gray-800 bg-gray-200">
+    <!--Left sidebar-->
+    <div class="flex flex-row w-auto flex-shrink-0">
+      <div class="flex flex-col items-center py-1 px-2 flex-shrink-0 bg-gray-50">
+        <a v-on:click="toggleTabs(1)" v-bind:class="{'border-2 border-orange-400 bg-orange-100': openTab === 1}"
+           class="flex items-center justify-center my-1 h-20 w-20 text-gray-400 rounded-md hover:bg-orange-200">
+          <svg viewBox="0 0 20 20" class="p-5" v-bind:class="{'fill-orange-400': openTab === 1, 'fill-gray-400': openTab !== 1}">
+            <path d="M10.862,6.47H3.968v6.032h6.894V6.47z M10,11.641H4.83V7.332H10V11.641z M12.585,11.641h-0.861v0.861h0.861V11.641z M7.415,14.226h0.862v-0.862H7.415V14.226z M8.707,17.673h2.586c0.237,0,0.431-0.193,0.431-0.432c0-0.237-0.193-0.431-0.431-0.431H8.707c-0.237,0-0.431,0.193-0.431,0.431C8.276,17.479,8.47,17.673,8.707,17.673 M5.691,14.226h0.861v-0.862H5.691V14.226z M4.83,13.363H3.968v0.862H4.83V13.363z M16.895,4.746h-3.017V3.023h1.292c0.476,0,0.862-0.386,0.862-0.862V1.299c0-0.476-0.387-0.862-0.862-0.862H10c-0.476,0-0.862,0.386-0.862,0.862v0.862c0,0.476,0.386,0.862,0.862,0.862h1.293v1.723H3.106c-0.476,0-0.862,0.386-0.862,0.862v12.926c0,0.476,0.386,0.862,0.862,0.862h13.789c0.475,0,0.861-0.387,0.861-0.862V5.608C17.756,5.132,17.369,4.746,16.895,4.746 M10.862,2.161H10V1.299h0.862V2.161zM11.724,1.299h3.446v0.862h-3.446V1.299z M13.016,4.746h-0.861V3.023h0.861V4.746z M16.895,18.534H3.106v-2.585h13.789V18.534zM16.895,15.088H3.106v-9.48h13.789V15.088z M15.17,12.502h0.862v-0.861H15.17V12.502z M13.447,12.502h0.861v-0.861h-0.861V12.502zM15.17,10.778h0.862V9.917H15.17V10.778z M15.17,9.055h0.862V8.193H15.17V9.055z M16.032,6.47h-4.309v0.862h4.309V6.47zM14.309,8.193h-0.861v0.862h0.861V8.193z M12.585,8.193h-0.861v0.862h0.861V8.193z M13.447,14.226h2.585v-0.862h-2.585V14.226zM13.447,10.778h0.861V9.917h-0.861V10.778z M12.585,9.917h-0.861v0.861h0.861V9.917z"/>
+          </svg>
+        </a>
+        <a v-on:click="toggleTabs(2)" v-bind:class="{'border-2 border-orange-400 bg-orange-100': openTab === 2}"
+           class="flex items-center justify-center my-1 h-20 w-20 text-gray-400 rounded-md hover:bg-orange-200">
+          <svg viewBox="0 0 20 20" class="p-5" v-bind:class="{'fill-orange-400': openTab === 2, 'fill-gray-400': openTab !== 2}">
+            <path d="M17.671,13.945l0.003,0.002l1.708-7.687l-0.008-0.002c0.008-0.033,0.021-0.065,0.021-0.102c0-0.236-0.191-0.428-0.427-0.428H5.276L4.67,3.472L4.665,3.473c-0.053-0.175-0.21-0.306-0.403-0.306H1.032c-0.236,0-0.427,0.191-0.427,0.427c0,0.236,0.191,0.428,0.427,0.428h2.902l2.667,9.945l0,0c0.037,0.119,0.125,0.217,0.239,0.268c-0.16,0.26-0.257,0.562-0.257,0.891c0,0.943,0.765,1.707,1.708,1.707S10,16.068,10,15.125c0-0.312-0.09-0.602-0.237-0.855h4.744c-0.146,0.254-0.237,0.543-0.237,0.855c0,0.943,0.766,1.707,1.708,1.707c0.944,0,1.709-0.764,1.709-1.707c0-0.328-0.097-0.631-0.257-0.891C17.55,14.182,17.639,14.074,17.671,13.945 M15.934,6.583h2.502l-0.38,1.709h-2.312L15.934,6.583zM5.505,6.583h2.832l0.189,1.709H5.963L5.505,6.583z M6.65,10.854L6.192,9.146h2.429l0.19,1.708H6.65z M6.879,11.707h2.027l0.189,1.709H7.338L6.879,11.707z M8.292,15.979c-0.472,0-0.854-0.383-0.854-0.854c0-0.473,0.382-0.855,0.854-0.855s0.854,0.383,0.854,0.855C9.146,15.596,8.763,15.979,8.292,15.979 M11.708,13.416H9.955l-0.189-1.709h1.943V13.416z M11.708,10.854H9.67L9.48,9.146h2.228V10.854z M11.708,8.292H9.386l-0.19-1.709h2.512V8.292z M14.315,13.416h-1.753v-1.709h1.942L14.315,13.416zM14.6,10.854h-2.037V9.146h2.227L14.6,10.854z M14.884,8.292h-2.321V6.583h2.512L14.884,8.292z M15.978,15.979c-0.471,0-0.854-0.383-0.854-0.854c0-0.473,0.383-0.855,0.854-0.855c0.473,0,0.854,0.383,0.854,0.855C16.832,15.596,16.45,15.979,15.978,15.979 M16.917,13.416h-1.743l0.189-1.709h1.934L16.917,13.416z M15.458,10.854l0.19-1.708h2.218l-0.38,1.708H15.458z"/>
+          </svg>
+        </a>
+      </div>
+    </div>
+    <!--left sidebar end-->
+    <!--tab page content-->
+    <div class="flex-grow flex w-full">
+      <!--product list-->
+      <div class="flex flex-col h-full w-full p-4" v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+        <h1>SEARCH PRODUCT</h1>
+      </div>
+      <div class="flex flex-col h-full w-full p-4" v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+        <h1>WAITING EMPLOYERS</h1>
+      </div>
+      <!--product list end-->
+    </div>
+    <!--tab page content end-->
+    <!--shopping cart-->
+    <div class="flex flex-col h-full w-5/12 bg-gray-50 border-l-2 border-gray-300">
+      <div class="border-b-2 w-full h-24 border-gray-300 items-center flex p-4">
+        <div class="border-2 rounded-lg flex-row flex items-center p-1 border-gray-200">
+          <svg viewBox="-4 0 32 32" class="w-14">
+            <path d="M0 25.281h0.781v-18.563h-0.781v18.563zM2.344 25.281h1.531v-18.563h-1.531v18.563zM5.406 25.281h1.563v-18.563h-1.563v18.563zM8.5 25.281h3.125v-18.563h-3.125v18.563zM13.156 25.281h2.344v-18.563h-2.344v18.563zM17.031 25.281h1.563v-18.563h-1.563v18.563zM20.125 25.281h0.781v-18.563h-0.781v18.563zM22.469 25.281h1.531v-18.563h-1.531v18.563z" class="fill-gray-500"/>
+          </svg>
+          <input class="text-2xl h-12 p-2 bg-gray-200 rounded-md"/>
+        </div>
+      </div>
+      <!--items list area-->
+      <div class="flex flex-grow w-full h-3/5">
+        <ul class="w-full overflow-y-scroll p-2 list-shoppingcart">
+          <ShoppingCartItem :title="'dsadsa'"/>
+          <ShoppingCartItem :title="'dsa'"/>
+          <ShoppingCartItem :title="'dsadhgfdghfsa'"/>
+          <ShoppingCartItem :title="'jghjgfjh'"/>
+          <ShoppingCartItem :title="'dsa'"/>
+          <ShoppingCartItem :title="'dsadhgfdghfsa'"/>
+          <ShoppingCartItem :title="'jghjgfjh'"/>
+          <ShoppingCartItem :title="'dsa'"/>
+          <ShoppingCartItem :title="'dsadhgfdghfsa'"/>
+          <ShoppingCartItem :title="'jghjgfjh'"/>
+          <ShoppingCartItem :title="'dsa'"/> 
+          <ShoppingCartItem :title="'dsadhgfdghfsa'"/>
+          <ShoppingCartItem :title="'jghjgfjh'"/>
+        </ul>
+      </div>
+      <!--items list area end-->
+      <div class="flex flex-col w-full h-3/6 bg-gray-200">
+        <div class="flex flex-col w-full h-full">
+          <h1>TOPLAM FİYAT VS</h1>
+        </div>
+        <div class="flex flex-row w-full h-auto p-2">
+          <button class="w-20 h-20 bg-orange-500 rounded-md p-2 mx-2">
+            <svg class="fill-gray-200" viewBox="0 0 16 16">
+             <path d="M14.75 2.5H1.25A1.25 1.25 0 0 0 0 3.75v8.5a1.25 1.25 0 0 0 1.25 1.25h13.5A1.25 1.25 0 0 0 16 12.25v-8.5a1.25 1.25 0 0 0-1.25-1.25zM1.25 3.75h2a1.86 1.86 0 0 1-2 1.75zm0 8.5V10.5a1.86 1.86 0 0 1 2 1.75zm13.5 0H12.8a1.86 1.86 0 0 1 1.95-1.75zm0-3a3.1 3.1 0 0 0-3.2 3h-7.1a3.1 3.1 0 0 0-3.2-3v-2.5a3.1 3.1 0 0 0 3.2-3h7.1a3.1 3.1 0 0 0 3.2 3zm0-3.75a1.86 1.86 0 0 1-1.95-1.75h1.95z"></path>
+             <path d="M8 5a3.1 3.1 0 0 0-3.2 3A3.1 3.1 0 0 0 8 11a3.1 3.1 0 0 0 3.2-3A3.1 3.1 0 0 0 8 5zm0 4.75A1.86 1.86 0 0 1 6.05 8 1.86 1.86 0 0 1 8 6.25 1.86 1.86 0 0 1 10 8a1.86 1.86 0 0 1-2 1.75z"></path>
+            </svg>
+          </button>
+          <button class="w-20 h-20 bg-lime-600 rounded-md p-2">
+            <svg class="fill-gray-200" viewBox="0 0 24 24">
+               <path d="M16,6 L20,6 C21.1045695,6 22,6.8954305 22,8 L22,16 C22,17.1045695 21.1045695,18 20,18 L16,18 L16,19.9411765 C16,21.0658573 15.1177541,22 14,22 L4,22 C2.88224586,22 2,21.0658573 2,19.9411765 L2,4.05882353 C2,2.93414267 2.88224586,2 4,2 L14,2 C15.1177541,2 16,2.93414267 16,4.05882353 L16,6 Z M20,11 L16,11 L16,16 L20,16 L20,11 Z M14,19.9411765 L14,4.05882353 C14,4.01396021 13.9868154,4 14,4 L4,4 C4.01318464,4 4,4.01396021 4,4.05882353 L4,19.9411765 C4,19.9860398 4.01318464,20 4,20 L14,20 C13.9868154,20 14,19.9860398 14,19.9411765 Z M5,19 L5,17 L7,17 L7,19 L5,19 Z M8,19 L8,17 L10,17 L10,19 L8,19 Z M11,19 L11,17 L13,17 L13,19 L11,19 Z M5,16 L5,14 L7,14 L7,16 L5,16 Z M8,16 L8,14 L10,14 L10,16 L8,16 Z M11,16 L11,14 L13,14 L13,16 L11,16 Z M13,5 L13,13 L5,13 L5,5 L13,5 Z M7,7 L7,11 L11,11 L11,7 L7,7 Z M20,9 L20,8 L16,8 L16,9 L20,9 Z"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+    <!--shopping cart end-->
+  </div>
+  <!--no print area end-->
+</template>
+<style scoped>
+  .list-shoppingcart > li:nth-child(odd) {
+    --tw-bg-opacity: 1;
+    background-color: rgba(238, 238, 238, var(--tw-bg-opacity));
+  }
+</style>
---
+++ src/components/ShoppingCartItem.vue
@@ -0,0 +1,59 @@
+<script setup>
+    defineProps(['title'])
+</script>
+<script>
+    export default {
+        name: 'ShoppingCartItem',
+        data() {
+            return {
+                isOpen: false
+            }
+        },
+        methods: {
+            toggleOpen() {
+                if(event.target.tagName === "INPUT")
+                {
+                    return;
+                }
+                this.isOpen = !this.isOpen
+            }
+
+        }
+    }
+</script>
+<template>
+    <li @click="toggleOpen" class="list-none w-full flex flex-col rounded-xl pt-3 mb-3" v-bind:class="{'border-l-4 border-lime-500': isOpen}">
+        <div class="flex flex-row w-full ">
+            <svg viewBox="0 0 20 34" class="w-8 h-8 stroke-gray-400" v-if="!isOpen">
+                <path d="M6 18M18 12L13 7M18 12L13 17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+            <svg viewBox="0 0 20 34" class="w-8 h-8 stroke-gray-400" v-if="isOpen">
+                 <path d="M12 18L7 13M12 18L17 13" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
+            </svg>
+            <span class="pl-3 -mt-0.5">50</span>
+            <div class="flex flex-col ml-3 -mt-0.5 w-full">
+                <h2 class="text-slate-900">{{ title }}</h2>
+                <h3 class="text-gray-400 text-sm">Renk</h3>
+            </div>
+            <div class="w-2/6 h-full text-center flex flex-col">
+                <span class="text-center">5.00 TL</span>
+            </div>
+            <button class="w-10 mr-2">
+                <svg viewBox="0 0 24 37">
+                    <path fill="none" d="M12.0004 9.5L17.0004 14.5M17.0004 9.5L12.0004 14.5M4.50823 13.9546L7.43966 17.7546C7.79218 18.2115 7.96843 18.44 8.18975 18.6047C8.38579 18.7505 8.6069 18.8592 8.84212 18.9253C9.10766 19 9.39623 19 9.97336 19H17.8004C18.9205 19 19.4806 19 19.9084 18.782C20.2847 18.5903 20.5907 18.2843 20.7824 17.908C21.0004 17.4802 21.0004 16.9201 21.0004 15.8V8.2C21.0004 7.0799 21.0004 6.51984 20.7824 6.09202C20.5907 5.71569 20.2847 5.40973 19.9084 5.21799C19.4806 5 18.9205 5 17.8004 5H9.97336C9.39623 5 9.10766 5 8.84212 5.07467C8.6069 5.14081 8.38579 5.2495 8.18975 5.39534C7.96843 5.55998 7.79218 5.78846 7.43966 6.24543L4.50823 10.0454C3.96863 10.7449 3.69883 11.0947 3.59505 11.4804C3.50347 11.8207 3.50347 12.1793 3.59505 12.5196C3.69883 12.9053 3.96863 13.2551 4.50823 13.9546Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </button>
+        </div>
+        <div v-if="isOpen" class="w-full flex flex-row p-4">
+            <div class="flex flex-col w-4/12">
+                <span>Count</span>
+                <input class="rounded-md border-2 border-gray-400 p-2 text-right w-32">
+            </div>
+            <div class="flex flex-col w-4/12">
+                <span>Unit Price</span>
+                <input class="rounded-md border-2 border-gray-400 p-2 text-right w-32">
+            </div>
+        </div>
+    </li>
+</template>
+
